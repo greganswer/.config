2bcf4180d06025b5e433883fb0b624747e306a9a
Greg Answer <greganswer@users.noreply.github.com>
2025-07-06 23:30:22 -0400

    Fix Neo-tree opens up the HOME directory sometimes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,3 +6,7 @@ local map = vim.keymap.set
 
 map({ "i", "n" }, "jk", "<Esc>:w<cr>") -- Press 'jk' rapidly to write and return to normal mode.
 map("n", "<leader><leader>", "<C-^>", { desc = "Toggle last files", noremap = true })
+
+-- Because Neo-tree opens up the HOME directory sometimes
+vim.keymap.del("n", "<leader>e") -- remove default
+map("n", "<leader>e", "<leader>E", { remap = true, desc = "Neo-tree (cwd, alias)" })
